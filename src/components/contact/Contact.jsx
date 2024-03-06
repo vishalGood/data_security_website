c2380d096ec9a2b0286ccaaca76bd13fcdb9d86f
vishalGood <vishalgoodart@gmail.com>
2024-03-06 17:16:32 +0530

    first commit

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './ContactStyles.css'
+
+const Contact = () => {
+  
+    return (
+        <div className='contact'>
+            <div className="container">
+                <div className="form-container">
+                    <form onSubmit={(e)=> e.preventDefault()}>
+                        <h1><span>Contact</span> Us</h1>
+                        <div>
+                            <label>Name</label>
+                            <input type="text" placeholder='Enter your name' />
+                        </div>
+                        <div>
+                            <label>Email</label>
+                            <input type="email" placeholder='Enter your email' />
+                        </div>
+                        <div>
+                            <label>Message</label>
+                            <textarea style={{ resize: "none" }} rows='10' placeholder='Enter your message' />
+                        </div>
+                        <button>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
